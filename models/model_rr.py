Fix RMSE calculation using np.sqrt

models/model_rr.py
@@ -22,7 +22,7 @@ def run_ridge_regression(df_train, df_test, numerical_features, categorical_feat
     y_pred = pipe.predict(Xte)
     
     mae = mean_absolute_error(y_te, y_pred)
-    rmse = mean_squared_error(y_te, y_pred, squared=False)
+    rmse = np.sqrt(mean_squared_error(y_te, y_pred))
     r2 = r2_score(y_te, y_pred)
     
     results = pd.DataFrame({'predicted': y_pred, 'observed': y_te})